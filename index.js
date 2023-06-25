0ac03d2f0c889b125415c4cbf220f92bbd679e17
tuckl <tuckerlarrabee13@gmail.com>
2023-06-25 17:02:35 -0400

    working on hamburger scroll bug

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 let artistSearchBtn = document.querySelector(".artistSearchBtn")
-console.log("🚀 ~ file: index.js:2 ~ artistSearchBtn:", artistSearchBtn)
+let toggleOne = document.getElementById("toggle1")
+let menuOne = document.querySelector(".menu1")
+// menuOne.classlist.add()
+
+// function messWithToggle () {
+//   toggleOne.checked
+// }
+
 
 function play() {
-    var audio = document.getElementById("audio");
+    let audio = document.getElementById("audio");
     audio.play();
   }
 
 artistSearchBtn.addEventListener("click", (e) => {
   e.preventDefault()
-
   console.log("Hello")
+})
+
+window.addEventListener("scroll", () => {
+  if (toggleOne.checked) {
+    toggleOne.checked = false
+  }
 })
