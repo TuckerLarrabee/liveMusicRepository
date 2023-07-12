0bb5ff324fbbc422dad8e231c0445404751f521f
tuckl <tuckerlarrabee13@gmail.com>
2023-07-11 21:32:33 -0400

    google sheets call successful

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,35 @@
+//DOM elements
 let artistSearchBtn = document.querySelector(".artistSearchBtn")
 let toggleOne = document.getElementById("toggle1")
 
+// Google Sheets Fetch Call
+async function getGoogleSheetsData() {
+    console.log("in callGoogleSheets")
+    let sheetData = await fetch('http://localhost:3000/sheetData');
+    let sheetDataText = await sheetData.text()
+
+    // taking sheetDataText, removing first 49 characters, last 3 characters, 
+    // and all backslashes to allow for JSON parse
+    const sheetDataJsObj = sheetDataText.substr(49).slice(0, -3)
+    const sheetDataJSONObj = JSON.parse(sheetDataJsObj.replace(/\\/g, ''));
+
+    //sheetDataJSONObj.table and sheetDataJSONObj.rows have all data from query. Can loop thru
+    console.log("🚀 ~ file: index.js:23 ~ getGoogleSheetsData ~ jsObjNoSlash:", sheetDataJSONObj)
+
+    sheetDataJSONObj.table.rows.forEach(data => {
+      // console.log(data.c)
+      data.c.forEach(furtherData => {
+        console.log(furtherData.v)
+      })
+    })
+}
+
+
+// AWS S3 Call
 async function getAudioFilesS3 () {
   let audioFiles = await fetch ('http://localhost:3000/audioFiles')
   let audioFilesJSON = await audioFiles.json()
   console.log("🚀 ~ file: index.js:7 ~ getAudioFilesS3 ~ audioFilesJSON:", audioFilesJSON)
-
-  audioFilesJSON.forEach(val => {
-    console.log(val)
-    console.log(val.split('/'))
-    console.log(val.split('/').pop().split("_"))
-  })
 }
 
 artistSearchBtn.addEventListener("click", (e) => {
@@ -26,3 +45,4 @@ window.addEventListener("scroll", () => {
 })
 
 getAudioFilesS3()
+getGoogleSheetsData()
